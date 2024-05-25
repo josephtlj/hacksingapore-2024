500cba08c4bb0950850bd78ce3a1f07ebd04a778
Wei Ming <alvinwm0523@gmail.com>
2024-05-25 17:44:23 +0800

    Version 2
    flask and search adjustment with requirement adjustment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from flask import Flask, request, jsonify
 
 # Profile Button
 st.sidebar.button("Profile", on_click=lambda: st.experimental_rerun())
@@ -28,15 +29,16 @@ def filter_stations(query):
         filtered_df = mrtdata
     return filtered_df
 
+st.title('MRT Cabin Status Updates')
+
+# Convert station names to a list
+station_names = mrtdata['station_name'].tolist()
+
+# Search bar with autocomplete
+search_query = st.text_input("Search for an MRT station:", "")
+
 # App function
 def main():
-    st.title('MRT Cabin Status Updates')
-
-    # Convert station names to a list
-    station_names = mrtdata['station_name'].tolist()
-
-    # Search bar with autocomplete
-    search_query = st.text_input("Search for an MRT station:", "")
 
     if search_query:
         if search_query in station_names:
@@ -50,7 +52,7 @@ def main():
 main()
 
 
-from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
